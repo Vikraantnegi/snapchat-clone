18dee1e9fa61b9a35f233ed31eb6c2c0322b0cb4
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-11 01:45:56 +0530

    UPDATE: App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
-import WebCamCapture from './app/Components/WebCamCapture';
+import WebCamCapture from './app/Components/WebCamCapture/WebCamCapture';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ViewImage from './app/Components/ViewImage/ViewImage';
 
 function App() {
   return (
     <div className="App">
-      <WebCamCapture />
+      <Router>
+        <div className="snapchat-app">
+        <Switch>
+          <Route exact path="/">
+            <WebCamCapture />
+          </Route>
+          <Route exact path="/view">
+            <ViewImage />
+          </Route>
+        </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
