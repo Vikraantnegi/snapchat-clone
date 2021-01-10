c39eae7edad629bc0302b268f2ba6c2a0f67a534
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-10 23:34:41 +0530

    UPDATE: App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WebCamCapture from './app/Components/WebCamCapture';
+
+function App() {
+  return (
+    <div className="App">
+      <WebCamCapture />
+    </div>
+  );
+}
+
+export default App;
