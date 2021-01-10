4bb294c760a8d4e595d2966e8f4864ba649a3e64
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-11 01:45:43 +0530

    Create ViewImage.js

---
+++ src/app/Components/ViewImage/ViewImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ViewImage.css';
+
+function ViewImage() {
+    return (
+        <div className="view-image">
+            
+        </div>
+    )
+}
+
+export default ViewImage
