14c0ed1fd5986619d8f6054b388d2de1cce24495
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-13 23:23:24 +0530

    UPDATE: SendButton

--- src/app/Components/ViewImage/ViewImage.js
+++ src/app/Components/ViewImage/ViewImage.js
@@ -4,6 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { resetImage, selectImage } from '../../../features/cameraSlice';
 import { useHistory } from 'react-router-dom';
 import { AttachFile, Close, Create, Crop, MusicNote, Note, Send, TextFields, Timer } from '@material-ui/icons';
+import {v4 as uuid} from 'uuid';
+import {db, storage} from '../../firebase';
+import firebase from 'firebase';
 
 function ViewImage() {
     const Image = useSelector(selectImage); 
@@ -19,6 +22,26 @@ function ViewImage() {
         dispatch(resetImage());
     }
 
+    const SendSnap = () => {
+        const id = uuid();
+        const uploadSnap = storage.ref(`posts/${id}`).putString(Image, 'data_url');
+        uploadSnap.on('state_changed', null, (err) => {
+            console.log(err);
+        }, () => {
+            storage.ref('posts').child(id).getDownloadURL()
+            .then(url => {
+                db.collection('posts').add({
+                    imgUrl : url,
+                    username: 'Waeed',
+                    read: false,
+                    // profilePic,
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                });
+                history.replace('/chats');
+            })
+        })
+    }
+
     return (
         <div className="view-image">
             <Close onClick={() => closeView()} className="close-icon" />
@@ -32,7 +55,7 @@ function ViewImage() {
                 <Timer />
             </div>
             <img src={Image} alt="cameraimage" />
-            <div className="snap-footer">
+            <div onClick={() => SendSnap()} className="snap-footer">
                 <h2>Send Now</h2>
                 <Send fontSize="small" className="send-icon" />
             </div>
